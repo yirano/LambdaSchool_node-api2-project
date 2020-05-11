8b25adcd1918d077b5cccdfaa4ee9b5f250c2e15
yirano <yirano@protonmail.ch>
2020-05-11 12:00:28 -0400

    Style posts list

--- client/src/Components/Nav/Navbar.jsx
+++ client/src/Components/Nav/Navbar.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const StyledUl = styled.ul`
+  display: flex;
+  justify-content: flex-end;
+`
+const styleLI = {
+  padding: '20px',
+  textDecoration: 'none'
+}
+
 
 const NavBar = () => {
   return (
-    <nav>
-      <ul>
-        <Link to="/">Home</Link>
-        <Link to="/posts">Posts</Link>
-        <Link to="/write">Write</Link>
-      </ul>
-    </nav>
+    <>
+      <nav>
+        <StyledUl>
+          <Link style={styleLI} to="/">Home</Link>
+          <Link style={styleLI} to="/posts">Posts</Link>
+          <Link style={styleLI} to="/write">Write</Link>
+        </StyledUl>
+      </nav>
+    </>
   )
 }
 
--- client/src/Components/Posts/Card.jsx
+++ client/src/Components/Posts/Card.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import { Card as ReactCard, CardTitle, CardText } from 'reactstrap'
 import { Button } from '@material-ui/core'
+
 
 const Card = ({ post, handleDelete }) => {
   console.log(post.id);
   return (
-    <div id={post.id}>
-      <h3>{post.title}</h3>
-      <p>{post.contents}</p>
-      <button id={post.id} onClick={e => handleDelete(e)}>Delete</button>
-    </div>
+    <ReactCard style={{ padding: '20px', margin: '20px 120px' }}>
+      <div id={post.id}>
+        <CardTitle>{post.title}</CardTitle>
+        <CardText>{post.contents}</CardText>
+        <Button id={post.id} onClick={e => handleDelete(e)}>Delete</Button>
+      </div>
+    </ReactCard>
   )
 }
 
